5fcac2a8a1e012eb830e3db7dc835bbc5b141434
MotieeAtefe <motiee1377@gmaile.com>
2024-10-10 20:24:44 +0330

    impleament method delegate

--- Program.cs
+++ Program.cs
@@ -1,10 +1,46 @@
-﻿namespace Delegate
+﻿using System;
+
+namespace Delegate
 {
     internal class Program
     {
+        public delegate double MathOperation(double a, double b);
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            MathOperation operation = AddOpration;
+
+
+            double result = operation(5, 10);
+            Console.WriteLine($"Result of Add: {result}"); // خروجی: Result of Add: 15
+
+            // تغییر delegate به متد دیگر
+            operation = SubtractOpration;
+            result = operation(10, 4);
+            Console.WriteLine($"Result of Subtract: {result}");
         }
+
+        static double AddOpration(double a, double b)
+        {
+            double result = a + b;
+            return result;
+        }
+        static double SubtractOpration(double a, double b)
+        {
+            double result = a - b;
+            return result;
+        }
+        static double MultyOpration(double a, double b)
+        {
+            double result = a * b;
+            return result;
+        }
+        double DivideOpration(double a, double b)
+        {
+            double result = a / b;
+            return result;
+        }
+
     }
+
+
 }
